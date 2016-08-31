737a102eef2ddae7ac916f1edee5d6110a65a24d
vertecx <pjr+git@petr.se>
2016-09-01 01:12:22 +0200

    Added code signing script for Windows

---
+++ Windows/CodeSign.ps1
@@ -0,0 +1,11 @@
+﻿Param (
+    [Parameter(Mandatory=$true)]
+    [string]$Path,
+    [Parameter(Mandatory=$true)]
+    [string]$Certificate
+)
+
+# signtool claims setup.exe is not a valid Win32 application so don't try to sign it.
+Get-ChildItem -Path $Path -Recurse -Include *.dll,*.msi  | ForEach-Object {
+    & signtool.exe sign /fd SHA256 /n "$Certificate" /tr "http://tsa.startssl.com/rfc3161" $_.FullName
+}
